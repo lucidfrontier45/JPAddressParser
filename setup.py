2cf8b6fb19599c59c909d334c76647c69c311687
Du Shiqiao <lucidfrontier.45@gmail.com>
2021-09-09 00:18:52 +0900

    add project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = ["jpaddressparser"]
+
+setup_kwargs = {
+    "name": "jpaddressparser",
+    "packages": packages,
+}
+
+setup(**setup_kwargs)
